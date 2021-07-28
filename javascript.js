07f6796b191f44f14e25fb2c6e354a7118c1c26c
marlingr7 <marlin.gu7@gmail.com>
2021-07-28 17:21:15 -0500

    templates

--- javascript.js
+++ javascript.js
@@ -4,28 +4,47 @@ let btnGifsOnly = document.getElementById("btn-all-gifs");
 let btnClearAll = document.getElementById("btn-clear-all");
 let imgs = document.getElementById("imgs");
 let divHidden = document.getElementsByClassName("show-hidde");
+let princ = document.getElementById("princ");
 
 async function createElements() {
   //Getting json
   let json = await getGifs(inputGifs.value);
+  inputGifs.style.background = "transparent";
+  inputGifs.style.fontSize = "12px";
+  inputGifs.style.borderRadius = "0";
+
   if (json.data.length == 0) {
     //No gifs found
-    let pImg = document.createElement("p");
-    var text = document.createTextNode(
-      "No gifs found. Please try something else!!!"
-    );
-    pImg.appendChild(text);
-    pImg.className = "p-img";
-    imgs.appendChild(pImg);
+    let text = "No gifs found. Please try something else!!!";
+    let divP = `
+        <p class="p-img"}>${text}</p>
+      `;
+    imgs.innerHTML = divP;
   } else {
+    let gifsImg = [];
     for (let i = 0; i < json.data.length; i++) {
-      let divImg = document.createElement("div");
-      divImg.style.backgroundImage =
-        "url(" + json.data[i].images.original.url + ")";
-      divImg.className = "img-div";
-      imgs.appendChild(divImg);
+      gifsImg.push(json.data[i].images.original.url);
     }
+    let divImg = `
+      <div class="img-div" style="background-image: url(${gifsImg[0]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[1]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[2]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[3]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[4]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[5]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[6]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[7]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[8]})"}></div>
+      <div class="img-div" style="background-image: url(${gifsImg[9]})"}></div>
+    `;
+    imgs.innerHTML = divImg;
   }
+}
+
+function showInput() {
+  inputGifs.style.background = "var(--line-color)";
+  inputGifs.style.fontSize = "12px";
+  inputGifs.style.borderRadius = "10px";
 }
 
 async function findGifs(e) {
@@ -35,9 +54,7 @@ async function findGifs(e) {
   //Validate input
   if (inputGifs.value == "") {
     inputGifs.setAttribute("placeholder", "you must look for some gif");
-    inputGifs.style.background = "var(--line-color)";
-    inputGifs.style.fontSize = "12px";
-    inputGifs.style.borderRadius = "10px";
+    showInput();
   } else {
     //Finding Gifs
     createElements();
@@ -59,7 +76,7 @@ function moveGifsFront() {
   }
 }
 
-function cleaning() {  
+function cleaning() {
   let imgLength = imgs.children.length;
   confetti.stop();
   for (let j = imgLength; j > 0; j--) {
@@ -67,11 +84,16 @@ function cleaning() {
   }
 }
 
-function cleaningAll() {  
+function cleaningAll() {
   cleaning();
   inputGifs.value = "";
+  inputGifs.setAttribute("placeholder", "find your gifs");
+  inputGifs.style.background = "transparent";
+  inputGifs.style.fontSize = "12px";
+  inputGifs.style.borderRadius = "0";
 }
 
+inputGifs.addEventListener("click", showInput, false);
 btnFindGifs.addEventListener("click", findGifs, false);
 btnGifsOnly.addEventListener("click", moveGifsFront, false);
 btnClearAll.addEventListener("click", cleaningAll, false);
